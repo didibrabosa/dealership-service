77822f9bf66c7fd2cfce8344df6388f142695758
Diego <diegovasconcelosb@gmail.com>
2025-05-20 15:47:14 -0300

    refactor: removing the repository injection from the service and leaving it as an int between router and repository

--- src/vehicle/repositories/vehicle.repository.ts
+++ src/vehicle/repositories/vehicle.repository.ts
@@ -1,12 +1,13 @@
 import { EntityManager, Repository } from "typeorm";
 import { VehicleEntity } from "../entities/vehicle.entity";
+import { VehicleRequestDto } from "../dtos/vehicle.request.dto";
 
 export class VehicleRepository extends Repository<VehicleEntity> {
     constructor(menager: EntityManager) {
         super(VehicleEntity, menager);
     }
 
-    createVehicle(vehicle: VehicleEntity) {
+    async createVehicle(vehicle: VehicleRequestDto): Promise<VehicleEntity> {
         const newVehicle = this.create(vehicle);
         return this.save(newVehicle);
     }
--- src/vehicle/services/vehicle.service.ts
+++ src/vehicle/services/vehicle.service.ts
@@ -1,29 +1,35 @@
 import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { VehicleEntity } from '../entities/vehicle.entity';
 import { VehicleRequestDto } from "../dtos/vehicle.request.dto";
+import { DataSource } from "typeorm";
+import { VehicleRepository } from "../repositories/vehicle.repository";
+import { VehicleResponseDto } from "../dtos/vehicle.response.dto";
+import { VehicleMapper } from "../mappers/vehicle.mapper";
 
 @Injectable()
 export class VehicleService {
+    
+    private vehicleRepository: VehicleRepository;
+
     constructor(
-        @InjectRepository(VehicleEntity)
-        private vehicleRepository: Repository<VehicleEntity>,
-    ) {}
-
-    async create(vehicleRequestDto: VehicleRequestDto): Promise<VehicleEntity> {
-        const vehicle = await this.vehicleRepository.create(vehicleRequestDto);
-        return this.vehicleRepository.save(vehicle);
-    }
-    findall(): Promise<VehicleEntity[]> {
-        return this.vehicleRepository.find();
+        private dataSource: DataSource
+    ) {
+        this.vehicleRepository = new VehicleRepository(dataSource.manager);
     }
 
-    findOne(id: string): Promise<VehicleEntity | null> {
-        return this.vehicleRepository.findOneBy({ id });
+    async createVehicle(request: VehicleRequestDto): Promise<VehicleResponseDto> {
+        const response = await this.vehicleRepository.createVehicle(request);
+        return VehicleMapper.toResponseDto(response);
     }
 
-    async remove(id: string): Promise<void> {
-        await this.vehicleRepository.delete(id)
-    }
+    //findall(): Promise<VehicleEntity[]> {
+    //    return this.vehicleRepository.find();
+    //}
+
+    //findOne(id: string): Promise<VehicleEntity | null> {
+    //    return this.vehicleRepository.findOneBy({ id });
+    //}
+
+    //async remove(id: string): Promise<void> {
+    //    await this.vehicleRepository.delete(id)
+   // }
 }
