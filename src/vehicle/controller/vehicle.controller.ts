0d7690cc3c95e9598fe12ba2c6c24714db2c4a8e
Diego <diegovasconcelosb@gmail.com>
2025-05-23 14:40:12 -0300

    remove: deleting the files of get-dtos because is not necessary anymore, cause the get requisitions is gonna be make in param not in the body

--- src/vehicle/controller/vehicle.controller.ts
+++ src/vehicle/controller/vehicle.controller.ts
@@ -1,9 +1,7 @@
-import { Body, Controller, Post, Get, Patch, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Delete } from '@nestjs/common';
 import { CreateVehicleDto, CreateVehicleResponseDto } from '../dtos/create-vehicle.dto';
 import { VehicleService } from '../services/vehicle.service';
 import { VehicleEntity } from '../entities/vehicle.entity';
-import { GetByTypeDto } from '../dtos/get-by-type.dto';
-import { GetByIdDto } from '../dtos/get-by-id.dtos';
 import { UpdateVehcileDto } from '../dtos/update-vehicle.dto';
 
 @Controller('vehicle')
@@ -13,8 +11,8 @@ export class VehicleController {
     ) {}
 
     @Post()
-    async createVehicle(@Body() request: CreateVehicleDto): Promise<CreateVehicleResponseDto> {
-        return await this.vehicleService.createVehicle(request);
+    async createVehicle(@Body() vehicle: CreateVehicleDto): Promise<CreateVehicleResponseDto> {
+        return await this.vehicleService.createVehicle(vehicle);
     }
 
     @Get()
@@ -22,18 +20,18 @@ export class VehicleController {
         return await this.vehicleService.getAllVehicles();
     }
 
-    @Get('type')
-    async getVehiclesByType(@Body() request: GetByTypeDto): Promise<VehicleEntity[]> {
-        return await this.vehicleService.getVehiclesByType(request.type);
+    @Get('/type/:typeVehicle')
+    async getVehiclesByType(@Param('typeVehicle') type: string): Promise<VehicleEntity[]> {
+        return await this.vehicleService.getVehiclesByType(type);
     }
 
-    @Get('id')
-    async getVehiclesById(@Body() request: GetByIdDto): Promise<VehicleEntity[]> {
-        return await this.vehicleService.getVehicleById(request.id);
+    @Get('/id/:idVehicle')
+    async getVehiclesById(@Param('idVehicle') id): Promise<VehicleEntity[]> {
+        return await this.vehicleService.getVehicleById(id);
     }
 
     @Patch(':id')
-    async updateVehicle(@Param('id') id: string, @Body() request: UpdateVehcileDto): Promise<VehicleEntity> {
+    async updateVehicle(@Param('id') id, @Body() request: UpdateVehcileDto): Promise<VehicleEntity> {
         return await this.vehicleService.updateVehiclePartialById(id, request)
     }
 }
--- src/vehicle/dtos/get-by-type.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from "class-validator";
-
-export class GetByTypeDto {
-    @IsNotEmpty()
-    @IsString()
-    type: string;
-}
