76743ee391e242ab97d5d7d0aefb3d01a55c2d19
Diego <diegovasconcelosb@gmail.com>
2025-05-23 10:39:02 -0300

    feat: adding a new dto to update a vehicle

---
+++ src/vehicle/dtos/update-vehicle.dto.ts
@@ -0,0 +1,27 @@
+import { IsOptional, IsString } from "class-validator";
+
+export class UpdateVehcileDto {
+    @IsOptional()
+    @IsString()
+    type: string;
+
+    @IsOptional()
+    @IsString()
+    brand: string;
+
+    @IsOptional()
+    @IsString()
+    model: string;
+    
+    @IsOptional()
+    @IsString()
+    color: string;
+    
+    @IsOptional()
+    @IsString()
+    year: string;
+
+    @IsOptional()
+    @IsString()
+    plate: string;
+}
