0e3b01e0b72a26e5096d0d461a93a6dcd842dbbb
Diego Barbosa Vasconcelos <diegovasconcelosb@gmail.com>
2025-07-25 14:27:56 -0300

    feat: adding propertys to swagger documentation

--- src/vehicle/dtos/create-vehicle.dto.ts
+++ src/vehicle/dtos/create-vehicle.dto.ts
@@ -1,31 +1,48 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsNumber, Length } from 'class-validator';
 
 export class CreateVehicleDto {
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The type of the vehicle',
+    example: 'Car',
+  })
   @IsString()
+  @IsNotEmpty()
   type: string;
 
-  @IsNotEmpty()
+  @ApiProperty({ description: 'The brand of the vehicle', example: 'Ford' })
   @IsString()
+  @IsNotEmpty()
   brand: string;
 
-  @IsNotEmpty()
+  @ApiProperty({ description: 'The model of the vehicle', example: 'Mustang' })
   @IsString()
+  @IsNotEmpty()
   model: string;
 
-  @IsNotEmpty()
+  @ApiProperty({ description: 'The color of the vehicle', example: 'Red' })
   @IsString()
+  @IsNotEmpty()
   color: string;
 
-  @IsNotEmpty()
-  @IsString()
-  year: string;
+  @ApiProperty({
+    description: 'The manufacturing year of the vehicle',
+    example: 2022,
+  })
+  @IsNumber()
+  year: number;
 
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The license plate of the vehicle',
+    example: 'ABC-1234',
+  })
   @IsString()
+  @IsNotEmpty()
+  @Length(7, 8)
   plate: string;
 }
 
 export class CreateVehicleResponseDto {
-  id: string;
+  @ApiProperty()
+  id: number;
 }
--- src/vehicle/dtos/update-vehicle.dto.ts
+++ src/vehicle/dtos/update-vehicle.dto.ts
@@ -1,27 +1,48 @@
-import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateVehcileDto {
+export class UpdateVehicleDto {
+  @ApiProperty({
+    description: 'The type of the vehicle',
+    example: 'Car',
+  })
   @IsOptional()
   @IsString()
   type: string;
 
+  @ApiProperty({ description: 'The brand of the vehicle', example: 'Ford' })
   @IsOptional()
   @IsString()
   brand: string;
 
+  @ApiProperty({ description: 'The model of the vehicle', example: 'Mustang' })
   @IsOptional()
   @IsString()
   model: string;
 
+  @ApiProperty({ description: 'The color of the vehicle', example: 'Red' })
   @IsOptional()
   @IsString()
   color: string;
 
+  @ApiProperty({
+    description: 'The manufacturing year of the vehicle',
+    example: 2022,
+  })
   @IsOptional()
-  @IsString()
-  year: string;
+  @IsNumber()
+  year: number;
 
+  @ApiProperty({
+    description: 'The license plate of the vehicle',
+    example: 'ABC-1234',
+  })
   @IsOptional()
   @IsString()
   plate: string;
+}
+
+export class UpdateVehicleResponseDto {
+  @ApiProperty()
+  id: number;
 }
